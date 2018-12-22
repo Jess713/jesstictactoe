bdd9aa00fc5edaf24da2635d232735873e9cbf4d
Jessica Kim <seung2613@gmail.com>
2018-12-22 02:26:02 -0800

    started working on win combo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 
 var camera, scene, renderer, mesh, material, controls;
-var targetList = [];
-var targetListBool = new Array(64).fill(false);
-  console.log(targetListBool);
+var red = new THREE.Color(0xff0000);
+var originalColor = new THREE.Color(0xadc9f4);
+var targetList = [
+  [[],[],[],[]],
+  [[],[],[],[]],
+  [[],[],[],[]],
+  [[],[],[],[]]
+];
+var intersects = [
+  [[],[],[],[]],
+  [[],[],[],[]],
+  [[],[],[],[]],
+  [[],[],[],[]]
+];
+
+console.log(targetList);
 
 // var projector, mouse = { x: 0, y: 0 };
 var projecter;
 var mouse = new THREE.Vector2(), INTERSECTED;
+
 init();
 animate();
 
@@ -18,51 +32,43 @@ function addCubes() {
   var xDistance = 30;
   var zDistance = 15;
   var geometry = new THREE.BoxBufferGeometry(10,10,10);
-  var material = new THREE.MeshBasicMaterial({color:0x6C70A8});
+  // var material = new THREE.MeshBasicMaterial({color:0x6C70A8});
 
-  //initial offset so does not start in middle.
-  var xOffset = -80;
-//1
+  //1
   for(let i = 0; i < 4; i++){
     for(let j = 0; j < 4; j++){
-      var mesh  = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial( { color: 0xadc9f4}));
-      mesh.position.x = (xDistance * (i)) + xOffset;
-      mesh.position.z = (zDistance * (j));
+      var mesh  = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial( { color: 0xadc9f4} ));
+      mesh.position.x = xDistance * i;
+      mesh.position.z = zDistance * j;
+      // mesh.position.y += 15;
       scene.add(mesh);
-      targetList.push(mesh);
+      targetList[i][j].push(mesh);
     }
-//2
+
     for(let j = 0; j < 4; j++){
-          var mesh2  = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial( { color: 0xadc9f4}));
-      mesh2.position.x = (xDistance * (i)) + xOffset;
-      mesh2.position.z = (zDistance * (j));
+      var mesh2  = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial( { color: 0xadc9f4} ));
+      mesh2.position.x = xDistance * i;
+      mesh2.position.z = zDistance * j;
       mesh2.position.y = 15;
       scene.add(mesh2);
-      targetList.push(mesh2);
+      targetList[i][j].push(mesh2);
     }
-//3
     for(let j = 0; j < 4; j++){
-          var mesh3  = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial( { color: 0xadc9f4}));
-      mesh3.position.x = (xDistance * (i)) + xOffset;
-      mesh3.position.z = (zDistance * (j));
+      var mesh3  = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial( { color: 0xadc9f4} ));
+      mesh3.position.x = xDistance * i;
+      mesh3.position.z = zDistance * j;
       mesh3.position.y = 30;
       scene.add(mesh3);
-      targetList.push(mesh3);
+      targetList[i][j].push(mesh3);
     }
-//4
     for(let j = 0; j < 4; j++){
-          var mesh4  = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial( { color: 0xadc9f4}));
-      mesh4.position.x = (xDistance * (i)) + xOffset;
-      mesh4.position.z = (zDistance * (j));
+      var mesh4  = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial( { color: 0xadc9f4} ));
+      mesh4.position.x = xDistance * i;
+      mesh4.position.z = zDistance * j;
       mesh4.position.y = 45;
       scene.add(mesh4);
-      targetList.push(mesh4);
+      targetList[i][j].push(mesh4);
     }
-  }
-
-
-  for (var i = 0; i < targetList.length; i++) {
-    targetList[i].name = i;
   }
 
 }
@@ -109,68 +115,84 @@ function init() {
 // initialize object to perform world/screen calculations
 projector = new THREE.Projector();
 // when the mouse moves, call the given function
-	document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 
 //HERE IS THE CLICK FUNCTION!!!---------------------------------------------
 function onDocumentMouseDown( event )
 {
-	// the following line would stop any other event handler from firing
-	// (such as the mouse's TrackballControls)
-	event.preventDefault();
+  // the following line would stop any other event handler from firing
+  // (such as the mouse's TrackballControls)
+  event.preventDefault();
 
-	console.log("Click.");
+  console.log("Clicked onDocumentMouseDown FUNCTION");
 
-	// update the mouse variable
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  // update the mouse variable
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
-	// find intersections
+  // find intersections
 
-	// create a Ray with origin at the mouse position
-	//   and direction into the scene (camera direction)
-	var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
-	projector.unprojectVector( vector, camera );
-	var ray = new THREE.Raycaster();
+  // create a Ray with origin at the mouse position
+  //   and direction into the scene (camera direction)
+  var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
+  projector.unprojectVector( vector, camera );
+  var ray = new THREE.Raycaster();
   ray.setFromCamera( mouse, camera );
 
-	// create an array containing all objects in the scene with which the ray intersects
-	var intersects = ray.intersectObjects( targetList );
+  console.log("------------------------------------------");
 
-	// if there is one (or more) intersections
-	if ( intersects.length > 0 && INTERSECTED != intersects[0].object)
-	{
+  // create an array containing all objects in the scene with which the ray intersects
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      for (let k = 0; k < 4; k++) {
+        intersects = ray.intersectObjects( scene.children );
+
+        targetList[i][j][k].name = i +" "+j+" "+k;
 
 
-      INTERSECTED = intersects[0].object;
-      INTERSECTED.material.emissive.setHex( 0xff0000 );
-    console.log(INTERSECTED.name);
+        // if there is one (or more) intersections
+        // It has something to do with your if condition. It's getting called 4 times here.
+        // I believe it has to do with this "INTERSECTED[0][0][k] != intersects[0].object"?
+        // But I'm not too sure what it's doing.
+        if ( intersects.length > 0 && INTERSECTED != intersects[0].object)
+        {
 
-		// console.log("Hit @ " + toString( intersects[0].point ) );
-		// change the color of the closest face.
-		// intersects[ 0 ].face.color.setHex(0xffa500);
-		// intersects[ 0 ].object.geometry.colorsNeedUpdate = true;
-    for (var i = 0; i < targetList.length; i++) {
 
-      if (INTERSECTED.name==i) {
-        targetListBool[i] = true;
+          INTERSECTED = intersects[0].object;
+
+          // INTERSECTED.id= targetList[i][j][k].name;
+          INTERSECTED.material.emissive.setHex( 0xff0000 );
+          // let val = document.getElementById(INTERSECTED.name);
+          console.log("The Intersected Node: " + INTERSECTED.name);
+
+
+
+        }
+//Checks Horizontal win on the first plane.
+        if(targetList[0][j][k].material.emissive.equals({r:1,g:0,b:0})
+          &&targetList[0][j+1][k].material.emissive.equals({r:1,g:0,b:0})
+          &&targetList[0][j+2][k].material.emissive.equals({r:1,g:0,b:0})
+          &&targetList[0][j+3][k].material.emissive.equals({r:1,g:0,b:0})){
+          alert('red win');
+        }
 
       }
     }
-    console.log(targetListBool);
-	}
-//THis is the win condition for now, but i'll make into 3d array like you did!
-  for (let i = 0; i <targetListBool.length ; i+=4) {
-    if(targetListBool[i]
-    &&targetListBool[i+1]
-    &&targetListBool[i+2]
-    &&targetListBool[i+3]){
-        console.log('win');
-        alert('win!!!!!!!!!!!!');
-    }
   }
 
+  console.log(targetList);
+
+
+
+
+
+
+
 }
-//////////////////////////////////////
+//CLICK Function Ended--------------//////////////////////////////////////
+
+
+
 
 
 
